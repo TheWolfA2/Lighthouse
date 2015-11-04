aa6c9e44aa67e9d6cc71d7699fc25f0a9732ad8b
Billy Wolfington <man_of_magik903@yahoo.com>
2015-11-03 22:22:28 -0600

    Added a basic player controller class

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerCharacter CharacterData;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
