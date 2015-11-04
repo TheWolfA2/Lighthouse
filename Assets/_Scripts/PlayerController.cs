e8190e5456b724cecf303453d725e7daa3ea0da3
Billy Wolfington <man_of_magik903@yahoo.com>
2015-11-04 00:19:55 -0600

    Simple movement on the X and Z axes

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public float XAxisSpeed = 5;
+    public float ZAxisSpeed = 5;
+
     public PlayerCharacter PlayerData;
 
     // Use this for initialization
@@ -12,14 +15,42 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
-    void FixedUpdate()
+    void Update()
     {
-        Debug.Log("Name: " + PlayerData.CharacterName);
-        Debug.Log("Attributes: " + PlayerData.Attributes.ToString());
+        if (IsMovementPressed())
+        {
+            int directionX = GetDirection(Input.GetAxisRaw("Horizontal")),
+                directionZ = GetDirection(Input.GetAxisRaw("Vertical"));
+            Move(XAxisSpeed * Time.deltaTime * directionX, ZAxisSpeed * Time.deltaTime * directionZ);
+        }
+    }
+
+    bool IsMovementPressed()
+    {
+        return Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0;
+    }
+
+    int GetDirection(float axisVal)
+    {
+        if (axisVal < 0)
+        {
+            return -1;
+        }
+        else if (axisVal > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    void Move(float dX, float dZ)
+    {
+        Vector3 newPos = transform.position;
+        newPos.x += dX;
+        newPos.z += dZ;
+        transform.position = newPos;
     }
 }
